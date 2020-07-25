02e43046fcb52a6eb0b0f66a825fe9c6e7d19972
anishkapuskar <kapuskar.a@husky.neu.edu>
2020-07-24 20:18:38 -0400

    a9 changes

--- LambdaApp/src/main/java/lambdaapp.java
+++ LambdaApp/src/main/java/lambdaapp.java
@@ -1,118 +1,87 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import org.json.JSONObject;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.*;
-import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
-import com.amazonaws.services.simpleemail.model.*;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 
-
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Calendar;
 public class lambdaapp implements RequestHandler<SNSEvent, Object> {
 
-    static DynamoDB dynamoDB;
-    String domain = "prod.anishkapuskar.me";
-
-
+    static Context context = null;
+    static LambdaLogger logger = null;
 
     public Object handleRequest(SNSEvent request, Context context) {
-
-
-
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
-
+        lambdaapp.context = context;
+        lambdaapp.logger = context.getLogger();
         context.getLogger().log("Invocation started: " + timeStamp);
 
-        context.getLogger().log("1: " + (request == null));
-
-        context.getLogger().log("2: " + (request.getRecords().size()));
+        String domain="prod.anishkapuskar.me";
+        JSONObject body = new JSONObject(request.getRecords().get(0).getSNS().getMessage());
 
         context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
+        dynamodb dbop = new dynamodb();
+        String token = dbop.getToken(body.getString("email"));
+        //if(token!=null)
+        sendEmail(body.getString("email"), domain, token);
 
         timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
-
-        try {
-            context.getLogger().log("trying to connect to dynamodb");
-            init();
-            long unixTime = Instant.now().getEpochSecond()+20*60;
-            Table table = dynamoDB.getTable("csye6225");
-            if(table == null)
-                context.getLogger().log("table is culprit");
-            context.getLogger().log("Above Item");
-            //context.getLogger().log("Table Description: "+table.getDescription().getTableName());
-            Item item = table.getItem("username", request.getRecords().get(0).getSNS().getMessage());
-            context.getLogger().log("Below Item");
-
-            if(item==null) {
-                Item itemPut = new Item()
-                        .withPrimaryKey("username", request.getRecords().get(0).getSNS().getMessage())//string id
-                        .withString("token", context.getAwsRequestId())
-                        .withNumber("passwordTokenExpiry", unixTime);
-
-                table.putItem(itemPut);
-
-                try {
-                    String FROM = "password_reset@"+domain;
-                    String TO = request.getRecords().get(0).getSNS().getMessage();
-                    String token = request.getRecords().get(0).getSNS().getMessageId();
-                    AmazonSimpleEmailService client =
-                            AmazonSimpleEmailServiceClientBuilder.standard()
-                                    .withRegion(Regions.US_EAST_1).build();
-                    SendEmailRequest req = new SendEmailRequest()
-                            .withDestination(
-                                    new Destination()
-                                            .withToAddresses(TO))
-                            .withMessage(
-                                    new Message()
-                                            .withBody(
-                                                    new Body()
-                                                            .withHtml(
-                                                                    new Content()
-                                                                            .withCharset(
-                                                                                    "UTF-8")
-                                                                            .withData(
-                                                                                    "Please click on the below link to reset the password<br/>"+
-                                                                                            "<p><a href='#'>https://"+domain+"/reset?email="+TO+"&token="+token+"</a></p>"))
-                                            )
-                                            .withSubject(
-                                                    new Content().withCharset("UTF-8")
-                                                            .withData("Password Reset Link")))
-                            .withSource(FROM);
-                    SendEmailResult response = client.sendEmail(req);
-                    context.getLogger().log ("Email sent!");
-                } catch (Exception ex) {
-                    context.getLogger().log ("The email was not sent. Error message: "
-                            + ex.getMessage());
-                }
-            }else{
-                context.getLogger().log("-----------------------Item with id "+request.getRecords().get(0).getSNS().getMessage()+" already exist-----------------------");
-                context.getLogger().log("Item with id "+request.getRecords().get(0).getSNS().getMessage()+" already exist");
-            }
-        }
-
-        catch(Exception e){
-            context.getLogger().log(e.getMessage());
-        }
-
         context.getLogger().log("Invocation completed: " + timeStamp);
-
         return null;
     }
 
-    private static void init() throws Exception {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1)
-                .build();
-        dynamoDB = new DynamoDB(client);
-    }
+    public void sendEmail(String TO, String domain, String token) {
 
+        String reset_url="http://"+domain+"/reset?email="+TO+"&token="+token;
+        String FROM = "password_reset@" + domain;
+
+        // The subject line for the email.
+        String SUBJECT = "Password Reset";
+
+        // The HTML body for the email.
+        String HTMLBODY = "<h2>Password Reset</h2>"
+                + "<p>Click <a href='"+reset_url+"'>"
+                + "here</a> to reset you password or, copy and paste the below url in your browser <br><br>"
+                + "<a href='"+reset_url+"'>" + reset_url+"</a>"
+                + "<br><p>This link will expire in "+ 15+" minute(s)";
+
+        // The email body for recipients with non-HTML email clients.
+        String TEXTBODY = "To reset your password copy and paste the following url in your browser:\n\n "
+                + reset_url+". This link will expire in "+ 15+" minute(s)";
+
+        try {
+            AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
+                    .withRegion(Regions.US_EAST_1).withCredentials(new DefaultAWSCredentialsProviderChain()).build();
+            SendEmailRequest request = new SendEmailRequest()
+                    .withDestination(new Destination()
+                            .withToAddresses(TO))
+                    .withMessage(new Message()
+                            .withBody(new Body()
+                                    .withHtml(new Content().withCharset("UTF-8")
+                                            .withData(HTMLBODY))
+                                    .withText(new Content().withCharset("UTF-8")
+                                            .withData(TEXTBODY)))
+                            .withSubject(new Content().withCharset("UTF-8")
+                                    .withData(SUBJECT)))
+                    .withSource(FROM);
+
+            client.sendEmail(request);
+            context.getLogger().log("Password reset email has been sent to " + TO);
+        } catch (Exception ex) {
+            System.out.println("The email was not sent. Error message: " + ex.getMessage());
+        }
+    }
 
 }
